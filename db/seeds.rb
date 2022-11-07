06ce4f39cbeece89d00958c5dc6f784abf767292
ganan-tamu <ganan@tamu.edu>
2022-11-06 19:09:52 -0600

    3rd commit: added my movie to seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,7 +17,9 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+    :release_date => '13-Aug-2021'},
+  {:title => 'Zootopia', :rating => 'G',
+    :release_date => '4-Mar-2016'}
 ]
 
 more_movies.each do |movie|
